cca7fdd883f5fba7abbb1f7af715c92564b2bfc7
Nick Desaulniers <ndesaulniers@google.com>
2016-02-23 14:12:49 -0800

    Silences WLAN, PCIe, and CPU suspend state kernel messages
    Bug: 27134656
    Change-Id: I681ec2171472514489365ca4bfc4ef16d9b344fe

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -601,7 +601,7 @@ static int pci_raw_set_power_state(struct pci_dev *dev, pci_power_t state)
 	dev->current_state = (pmcsr & PCI_PM_CTRL_STATE_MASK);
 	if (dev->current_state != state && printk_ratelimit())
 		dev_dbg(&dev->dev, "Refused to change power state, currently in D%d\n",
-			dev->current_state);
+			 dev->current_state);
 
 	/*
 	 * According to section 5.4.1 of the "PCI BUS POWER MANAGEMENT
